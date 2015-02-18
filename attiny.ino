f4fa8999c3ad6d163b1ec7a0436aff54f1b0adc0
Borja Escanilla Rodríguez <borja@November>
2015-02-18 10:59:50 +0100

    Primer commit

---
+++ attiny.ino
@@ -0,0 +1,44 @@
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+const int pinPIR  = 3;
+const int pinOPTO = 2;
+const int tiempoEncendido = 8;
+void setup() {
+
+  pinMode(pinPIR, INPUT);
+  pinMode(pinOPTO, OUTPUT);
+} // setup
+
+void sleep() {
+
+  GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
+  PCMSK |= _BV(PCINT3);                   // Use PB3 as interrupt pin
+  ADCSRA &= ~_BV(ADEN);                   // ADC off
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
+
+  sleep_enable();                         // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
+  sei();                                  // Enable interrupts
+  sleep_cpu();                            // sleep
+
+  cli();                                  // Disable interrupts
+  PCMSK &= ~_BV(PCINT3);                  // Turn off PB3 as interrupt pin
+  sleep_disable();                        // Clear SE bit
+  ADCSRA |= _BV(ADEN);                    // ADC on
+
+  sei();                                  // Enable interrupts
+} // sleep
+
+ISR(PCINT0_vect) {
+  // This is called when the interrupt occurs, but I don't need to do anything in it
+}
+
+void loop() {
+  sleep();
+  if(pinPIR == HIGH){
+    digitalWrite(pinOPTO, HIGH);
+    delay(tiempoEncendido*1000);
+    digitalWrite(pinOPTO, LOW);
+  }
+}
+
